ddebabd8f74714f9bf2dd4a08ffc495804ad63f4
brusus <rafael.fiallo93@gmail.com>
2026-09-12 18:45:49 +0200

    Revert "Bump version to 0.9.0-brusus.14"
    This reverts commit 8ce5a662787803ac53fb948e2e1c1b0cec4f1e2e.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,8 +104,8 @@ android {
         applicationId = "com.nuvio.tv"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1073
-        versionName = "0.9.0-brusus.14"
+        versionCode = 1072
+        versionName = "0.9.0-brusus.13"
 
         buildConfigField("String", "PARENTAL_GUIDE_API_URL", "\"${localProperties.getProperty("PARENTAL_GUIDE_API_URL", "")}\"")
         buildConfigField("String", "INTRODB_API_URL", "\"${localProperties.getProperty("INTRODB_API_URL", "")}\"")
